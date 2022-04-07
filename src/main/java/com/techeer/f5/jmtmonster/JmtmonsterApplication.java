4fdfee024325ec7bf1d1b08eb20c6e913a54142c
litsynp <nocte55is@gmail.com>
2022-04-07 18:16:20 +0900

    build: initialize project with Spring Initializr

---
+++ src/main/java/com/techeer/f5/jmtmonster/JmtmonsterApplication.java
@@ -0,0 +1,13 @@
+package com.techeer.f5.jmtmonster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmtmonsterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmtmonsterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techeer/f5/jmtmonster/JmtmonsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techeer.f5.jmtmonster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmtmonsterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

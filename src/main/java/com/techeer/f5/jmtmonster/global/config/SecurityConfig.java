e52eafd75a738320d55e001a1c82f472150c921a
litsynp <nocte55is@gmail.com>
2022-04-10 15:37:38 +0900

    feat: add security config

---
+++ src/main/java/com/techeer/f5/jmtmonster/global/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.techeer.f5.jmtmonster.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}

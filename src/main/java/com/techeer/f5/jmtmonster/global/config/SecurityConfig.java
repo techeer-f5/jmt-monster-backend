d74b1c27d960428d3af2c776fd40480804e42321
litsynp <nocte55is@gmail.com>
2022-04-10 15:43:46 +0900

    feat: enable CORS

--- src/main/java/com/techeer/f5/jmtmonster/global/config/SecurityConfig.java
+++ src/main/java/com/techeer/f5/jmtmonster/global/config/SecurityConfig.java
@@ -1,20 +1,44 @@
 package com.techeer.f5.jmtmonster.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Value("#{'${app.cors.allowed-origins}'}")
+    private List<String> corsAllowedOrigins;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and() // Enable CORS
                 .formLogin().disable() // Disable Spring Security login form
                 .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) // Send CSRF token in a cookie
         ;
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(corsAllowedOrigins);
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }

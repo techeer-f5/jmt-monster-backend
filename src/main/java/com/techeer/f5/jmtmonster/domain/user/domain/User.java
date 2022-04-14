84afcf80bc1189b9272f33a6b0e0367d6623ce70
jiho lee <optional.int@kakao.com>
2022-04-14 10:47:30 +0900

    fix: resolve wrong import

--- src/main/java/com/techeer/f5/jmtmonster/domain/user/domain/User.java
+++ src/main/java/com/techeer/f5/jmtmonster/domain/user/domain/User.java
@@ -13,7 +13,6 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.security.AuthProvider;
 import java.util.UUID;
 
 @Getter
@@ -67,7 +66,6 @@ public class User {
 
     @NotNull
     @Enumerated(EnumType.STRING)
-    // FIXME: implement AuthProvider
     private AuthProvider provider;
 
     public boolean addExtraInfo(String nickname, String address) throws IllegalStateException {

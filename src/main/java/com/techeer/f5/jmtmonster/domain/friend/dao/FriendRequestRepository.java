64fb3c9b548845cee8462ca4282ebf537e4495c3
litsynp <nocte55is@gmail.com>
2022-04-19 16:03:34 +0900

    feat: add friend request repository

---
+++ src/main/java/com/techeer/f5/jmtmonster/domain/friend/dao/FriendRequestRepository.java
@@ -0,0 +1,8 @@
+package com.techeer.f5.jmtmonster.domain.friend.dao;
+
+import com.techeer.f5.jmtmonster.domain.friend.domain.FriendRequest;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FriendRequestRepository extends JpaRepository<FriendRequest, UUID> {
+}

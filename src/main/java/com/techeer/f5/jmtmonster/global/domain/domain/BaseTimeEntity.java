8aa8ae3e02a137e1e7e98758592b78b23e22bb93
litsynp <nocte55is@gmail.com>
2022-04-19 16:01:43 +0900

    feat: add abstract BaseTimeEntity and DTO

---
+++ src/main/java/com/techeer/f5/jmtmonster/global/domain/domain/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package com.techeer.f5.jmtmonster.global.domain.domain;
+
+import java.time.LocalDateTime;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdOn;
+
+    @LastModifiedDate
+    private LocalDateTime updatedOn;
+}

cf1aae5675652587a59a6dc2857144d29bfdb0c6
litsynp <nocte55is@gmail.com>
2022-04-29 18:51:21 +0900

    feat: add 422 inner resource not found exception

--- src/main/java/com/techeer/f5/jmtmonster/global/error/exception/InnerResourceNotFoundException.java
+++ src/main/java/com/techeer/f5/jmtmonster/global/error/exception/InnerResourceNotFoundException.java
@@ -1,14 +1,16 @@
 package com.techeer.f5.jmtmonster.global.error.exception;
 
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.validation.FieldError;
 
 @Setter
 @Getter
-public class ResourceNotFoundException extends CustomStatusException {
+public class InnerResourceNotFoundException extends CustomStatusErrorsException {
 
-    public ResourceNotFoundException(String resourceName, String fieldName, Object value) {
-
-        super(ErrorCode.NOT_FOUND, resourceName + " not found with " + fieldName + "=" + value.toString());
+    public InnerResourceNotFoundException(String resourceName, List<FieldError> errors) {
+        super(ErrorCode.INNER_RESOURCE_NOT_FOUND,
+                resourceName + " cannot be processed with given condition.", errors);
     }
 }

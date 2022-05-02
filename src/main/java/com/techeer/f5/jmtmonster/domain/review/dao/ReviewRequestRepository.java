a81b86fb1b75eb21e87698410ce5aa7925d31bdc
Wooseok Choe <chldntjr0425@gmail.com>
2022-05-03 01:36:24 +0900

    feat:add RequestReview repository

--- src/main/java/com/techeer/f5/jmtmonster/domain/review/dao/ReviewRequestRepository.java
+++ src/main/java/com/techeer/f5/jmtmonster/domain/review/dao/ReviewRequestRepository.java
@@ -1,4 +1,11 @@
 package com.techeer.f5.jmtmonster.domain.review.dao;
 
-public interface ReviewRequestRepository {
+import com.techeer.f5.jmtmonster.domain.friend.domain.FriendRequest;
+import com.techeer.f5.jmtmonster.domain.review.domain.ReviewRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ReviewRequestRepository extends JpaRepository<ReviewRequest, UUID> {
+    boolean existsById(UUID uuid);
 }
---
+++ src/main/java/com/techeer/f5/jmtmonster/domain/review/service/ReviewRequestService.java
@@ -0,0 +1,11 @@
+package com.techeer.f5.jmtmonster.domain.review.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewRequestService {
+
+}
+

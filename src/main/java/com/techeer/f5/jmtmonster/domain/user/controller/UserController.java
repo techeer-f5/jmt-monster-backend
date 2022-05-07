bf1ee03f4dad1671fbdd58ff5bff85a650f9b52b
jiho lee <optional.int@kakao.com>
2022-05-07 16:17:22 +0900

    feat: refactor UserController using UserService

--- src/main/java/com/techeer/f5/jmtmonster/domain/user/controller/UserController.java
+++ src/main/java/com/techeer/f5/jmtmonster/domain/user/controller/UserController.java
@@ -1,54 +1,33 @@
 package com.techeer.f5.jmtmonster.domain.user.controller;
 
-import com.techeer.f5.jmtmonster.domain.oauth.domain.PersistentToken;
-import com.techeer.f5.jmtmonster.domain.oauth.dto.UserResponseDto;
-import com.techeer.f5.jmtmonster.domain.oauth.repository.PersistentTokenRepository;
-import com.techeer.f5.jmtmonster.domain.user.dto.UserDto;
+import com.techeer.f5.jmtmonster.domain.user.dto.UserResponseDto;
+import com.techeer.f5.jmtmonster.domain.user.dto.ExtraUserInfoRequestDto;
+import com.techeer.f5.jmtmonster.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Optional;
-import java.util.UUID;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
-    private final PersistentTokenRepository persistentTokenRepository;
+    private final UserService userService;
 
     @GetMapping("/me")
     public ResponseEntity<UserResponseDto> getMyUser(HttpServletRequest request) {
-        UserResponseDto emptyDto = UserResponseDto.builder()
-                                    .success(false)
-                                    .user(null)
-                                    .build();
+        UserResponseDto userResponseDto = userService.getMyUser(request);
 
-        ResponseEntity<UserResponseDto> emptyResponse = ResponseEntity.status(HttpStatus.NOT_FOUND).body(emptyDto);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(userResponseDto);
+    }
 
-        String userId_ = (String) request.getAttribute("userId");
-
-        if (userId_ == null) {
-            return emptyResponse;
-        }
-
-        UUID userId = UUID.fromString(userId_);
-
-
-        Optional<PersistentToken> persistentToken = persistentTokenRepository.findById(userId);
-        if (persistentToken.isEmpty()) {
-            return emptyResponse;
-        }
-
-        UserResponseDto userResponseDto = UserResponseDto.builder()
-                                                .success(true)
-                                                .user(persistentToken.get()
-                                                        .getUser()
-                                                        .convert()).build();
+    @PostMapping("/me/extra-info")
+    public ResponseEntity<UserResponseDto> submitExtraInfo(HttpServletRequest request, @RequestBody @Valid ExtraUserInfoRequestDto extraUserInfoRequestDto) {
+        UserResponseDto userResponseDto = userService.submitExtraInfo(request, extraUserInfoRequestDto);
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(userResponseDto);

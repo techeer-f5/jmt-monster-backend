25995198b74efa6dc77bb24a0b3a8fff245095cc
jiho lee <optional.int@kakao.com>
2022-05-07 17:09:08 +0900

    fix: load ApplicationContext

--- src/test/java/com/techeer/f5/jmtmonster/domain/hello/HelloControllerTest.java
+++ src/test/java/com/techeer/f5/jmtmonster/domain/hello/HelloControllerTest.java
@@ -17,15 +17,18 @@ import com.techeer.f5.jmtmonster.domain.hello.dto.HelloRequestDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(HelloController.class)
+@SpringBootTest
 @AutoConfigureRestDocs
+@AutoConfigureMockMvc
 // For use test db
 @TestPropertySource("classpath:application-test.yml")
 public class HelloControllerTest {

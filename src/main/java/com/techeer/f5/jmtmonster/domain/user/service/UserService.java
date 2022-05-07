7a2f9c92e8f24e664db6519b8ff28caa5ce854a8
jiho lee <optional.int@kakao.com>
2022-05-07 16:16:43 +0900

    feat: add UserService

---
+++ src/main/java/com/techeer/f5/jmtmonster/domain/user/service/UserService.java
@@ -0,0 +1,95 @@
+package com.techeer.f5.jmtmonster.domain.user.service;
+
+import com.techeer.f5.jmtmonster.domain.oauth.domain.PersistentToken;
+import com.techeer.f5.jmtmonster.domain.user.dto.UserResponseDto;
+import com.techeer.f5.jmtmonster.domain.oauth.repository.PersistentTokenRepository;
+import com.techeer.f5.jmtmonster.domain.user.domain.User;
+import com.techeer.f5.jmtmonster.domain.user.dto.ExtraUserInfoRequestDto;
+import com.techeer.f5.jmtmonster.domain.user.dto.UserDto;
+import com.techeer.f5.jmtmonster.domain.user.dto.UserMapper;
+import com.techeer.f5.jmtmonster.domain.user.repository.UserRepository;
+import com.techeer.f5.jmtmonster.global.error.exception.NotAuthorizedException;
+import com.techeer.f5.jmtmonster.global.error.exception.ResourceNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PersistentTokenRepository persistentTokenRepository;
+    private final UserMapper userMapper;
+
+    public UUID getTokenId(HttpServletRequest request) {
+
+        String userId = (String) request.getAttribute("userId");
+
+        if (userId == null) {
+            throw new NotAuthorizedException("토큰이 주어지지 않았습니다.");
+        }
+
+        try {
+            return UUID.fromString(userId);
+        } catch (IllegalArgumentException exception) {
+            log.error(exception.toString());
+            throw new NotAuthorizedException("토큰 UUID 파싱에 실패했습니다.");
+        }
+    }
+
+    public Optional<User> findUserById(UUID id) {
+        return userRepository.findById(id);
+    }
+
+    public Optional<User> findUserByTokenId(UUID tokenId) {
+        Optional<PersistentToken> optionalPersistentToken = persistentTokenRepository.findById(tokenId);
+
+        if (optionalPersistentToken.isEmpty()) {
+            return Optional.empty();
+        }
+
+        PersistentToken persistentToken = optionalPersistentToken.get();
+
+        return Optional.ofNullable(persistentToken.getUser());
+    }
+
+    public User findUserWithRequest(HttpServletRequest request) {
+        UUID tokenId = getTokenId(request);
+        Optional<User> optionalUser = findUserByTokenId(tokenId);
+
+        if (optionalUser.isEmpty()) {
+            throw new ResourceNotFoundException("user", "tokenId", "");
+        }
+
+        return optionalUser.get();
+    }
+
+    public UserResponseDto getMyUser(HttpServletRequest request) {
+        User user = findUserWithRequest(request);
+
+        UserDto userDto = userMapper.toUserDto(user);
+
+        return UserResponseDto.builder()
+                .success(true)
+                .user(userDto)
+                .build();
+    }
+
+    public UserResponseDto submitExtraInfo(HttpServletRequest request, ExtraUserInfoRequestDto extraUserInfoRequestDto) {
+        User user = findUserWithRequest(request);
+
+        user.setNickname(extraUserInfoRequestDto.getNickname());
+        user.setAddress(extraUserInfoRequestDto.getAddress());
+        user.setImageUrl(extraUserInfoRequestDto.getImageUrl());
+        user.setExtraInfoInjected(true);
+
+        user.setVerified(user.getVerified() && user.getExtraInfoInjected());
+
+        return userMapper.toUserResponseDto(user);
+    }
+}

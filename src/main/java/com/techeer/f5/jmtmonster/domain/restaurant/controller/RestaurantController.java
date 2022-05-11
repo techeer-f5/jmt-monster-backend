f965db688c5dc6f1fe43b2edf3474868ef857426
gilhan <rlfgks97@gmail.com>
2022-05-12 01:33:01 +0900

    feat: add Restaurant Controller

---
+++ src/main/java/com/techeer/f5/jmtmonster/domain/restaurant/controller/RestaurantController.java
@@ -0,0 +1,35 @@
+package com.techeer.f5.jmtmonster.domain.restaurant.controller;
+
+import com.techeer.f5.jmtmonster.domain.restaurant.dto.mapper.RestaurantMapper;
+import com.techeer.f5.jmtmonster.domain.restaurant.dto.response.RestaurantResponseDto;
+import com.techeer.f5.jmtmonster.domain.restaurant.entity.Restaurant;
+import com.techeer.f5.jmtmonster.domain.restaurant.repository.RestaurantRepository;
+import com.techeer.f5.jmtmonster.domain.restaurant.service.RestaurantService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/restaurants")
+@RequiredArgsConstructor
+public class RestaurantController {
+
+    private final RestaurantService restaurantService;
+    private final RestaurantRepository restaurantRepository;
+
+    @PostMapping("/{cid}")
+    public ResponseEntity getRestaurantInformation(@PathVariable Long cid) {
+        RestaurantResponseDto responseDto;
+        if (restaurantRepository.existsByCid(cid)) {  // 정보가 있으면
+            responseDto = restaurantService.getRestaurantInfo(cid);
+        } else { // 정보가 없으면
+            responseDto = restaurantService.saveRestaurantInfo(cid);
+        }
+
+        return ResponseEntity.ok()
+                .body(responseDto);
+    }
+}

c09130eb61bf1b938e814d4fbaf3cd03aa7fc738
jiho lee <optional.int@kakao.com>
2022-05-17 00:23:25 +0900

    feat: use ObjectMapper bean in JsonMapper

--- src/main/java/com/techeer/f5/jmtmonster/global/utils/JsonMapper.java
+++ src/main/java/com/techeer/f5/jmtmonster/global/utils/JsonMapper.java
@@ -1,22 +1,29 @@
 package com.techeer.f5.jmtmonster.global.utils;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.test.web.servlet.MvcResult;
 
 @Component
+@RequiredArgsConstructor
 public class JsonMapper {
-    public static String asJsonString(final Object obj) {
+    public final ObjectMapper objectMapper;
+    public String asJsonString(final Object obj) {
         try {
-            final ObjectMapper mapper = new ObjectMapper();
-            return mapper.writeValueAsString(obj);
+            return objectMapper.writeValueAsString(obj);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static <T> T fromMvcResult(MvcResult result, Class<T> clazz) throws Exception {
-        return new ObjectMapper().readValue(result.getResponse().getContentAsString(), clazz);
+    public <T> T fromMvcResult(MvcResult result, Class<T> clazz) throws Exception {
+        return objectMapper.readValue(result.getResponse().getContentAsString(), clazz);
+    }
+
+    public <T> T fromMvcResult(MvcResult result, TypeReference<T> typeReference) throws Exception {
+        return objectMapper.readValue(result.getResponse().getContentAsString(), typeReference);
     }
 
 }

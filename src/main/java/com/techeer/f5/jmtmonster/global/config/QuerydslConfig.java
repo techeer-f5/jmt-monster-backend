d06e2b470383649f1db531af99c7093db90d5a71
litsynp <nocte55is@gmail.com>
2022-05-18 02:21:53 +0900

    feat: add QuerydslConfig

---
+++ src/main/java/com/techeer/f5/jmtmonster/global/config/QuerydslConfig.java
@@ -0,0 +1,19 @@
+package com.techeer.f5.jmtmonster.global.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuerydslConfig {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
+}

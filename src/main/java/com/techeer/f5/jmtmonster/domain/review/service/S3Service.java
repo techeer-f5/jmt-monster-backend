f6af47350831791ad2cb56f1e12957f98bbf456a
Wooseok Choe <chldntjr0425@gmail.com>
2022-05-18 22:20:30 +0900

    feat:add s3 service

--- src/main/java/com/techeer/f5/jmtmonster/domain/review/service/S3Service.java
+++ src/main/java/com/techeer/f5/jmtmonster/domain/review/service/S3Service.java
@@ -1,10 +1,29 @@
 package com.techeer.f5.jmtmonster.domain.review.service;
 
+import com.techeer.f5.jmtmonster.domain.review.domain.Updatable;
+import com.techeer.f5.jmtmonster.s3.util.S3ManagerImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class S3Service {
 
+    private S3ManagerImpl s3Manager;
+
+    public String uploadImage(MultipartFile multipartFile) throws IOException {
+        return s3Manager.uploadImage(multipartFile);
+    }
+
+    public String deleteByURL(String url){
+        return s3Manager.deleteByURL(url);
+    }
+
+    public String updateByURL(MultipartFile multipartFile, Updatable<String> updatable) throws IOException {
+        return s3Manager.updateByURL(multipartFile,updatable);
+    }
 }

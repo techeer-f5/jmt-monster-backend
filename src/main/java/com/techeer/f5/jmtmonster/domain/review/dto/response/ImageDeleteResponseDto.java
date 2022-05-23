58ee3de677983c8d130f4303c1edc18b69a052de
Wooseok Choe <chldntjr0425@gmail.com>
2022-05-23 19:31:19 +0900

    feat:Implement ImageResponseDto

--- src/main/java/com/techeer/f5/jmtmonster/domain/review/dto/response/ImageDeleteResponseDto.java
+++ src/main/java/com/techeer/f5/jmtmonster/domain/review/dto/response/ImageDeleteResponseDto.java
@@ -1,4 +1,15 @@
 package com.techeer.f5.jmtmonster.domain.review.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class ImageDeleteResponseDto {
+    @NotNull
+    String url;
 }
--- src/main/java/com/techeer/f5/jmtmonster/domain/review/dto/response/ImageUpdateResponseDto.java
+++ src/main/java/com/techeer/f5/jmtmonster/domain/review/dto/response/ImageUpdateResponseDto.java
@@ -1,4 +1,15 @@
 package com.techeer.f5.jmtmonster.domain.review.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class ImageUpdateResponseDto {
+    @NotNull
+    String url;
 }

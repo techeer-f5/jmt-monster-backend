8e24e9f0aaf13ef8a950b195446b243285947731
Wooseok Choe <chldntjr0425@gmail.com>
2022-05-23 19:41:24 +0900

    fix: make controller return http response message

--- src/main/java/com/techeer/f5/jmtmonster/domain/review/controller/S3Controller.java
+++ src/main/java/com/techeer/f5/jmtmonster/domain/review/controller/S3Controller.java
@@ -1,13 +1,22 @@
 package com.techeer.f5.jmtmonster.domain.review.controller;
 
 import com.techeer.f5.jmtmonster.domain.review.domain.Updatable;
+import com.techeer.f5.jmtmonster.domain.review.dto.request.ReviewRequestModel;
+import com.techeer.f5.jmtmonster.domain.review.dto.response.ImageCreateResponseDto;
+import com.techeer.f5.jmtmonster.domain.review.dto.response.ImageDeleteResponseDto;
+import com.techeer.f5.jmtmonster.domain.review.dto.response.ImageUpdateResponseDto;
 import com.techeer.f5.jmtmonster.domain.review.service.ReviewRequestService;
 import com.techeer.f5.jmtmonster.domain.review.service.S3Service;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 @RestController
 @RequestMapping("/api/v1/images")
@@ -17,18 +26,32 @@ public class S3Controller {
     private S3Service s3Service;
 
     @PostMapping
-    public String uploadImage(MultipartFile image) throws IOException {
-        return s3Service.uploadImage(image);
+    public ResponseEntity<ImageCreateResponseDto> uploadImage(@RequestBody MultipartFile image) throws IOException {
+        HttpHeaders headers= new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+        ImageCreateResponseDto dto = new ImageCreateResponseDto(s3Service.uploadImage(image));
+
+        return new ResponseEntity<>(dto, headers, HttpStatus.CREATED);
     }
 
     @DeleteMapping
-    public String deleteByURL(@RequestParam String url){
-        return s3Service.deleteByURL(url);
+    public ResponseEntity<ImageDeleteResponseDto> deleteByURL(@RequestParam String url){
+        HttpHeaders headers= new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+        ImageDeleteResponseDto dto = new ImageDeleteResponseDto(s3Service.deleteByURL(url));
+
+        return new ResponseEntity<>(dto, headers, HttpStatus.OK);
     }
 
     @PutMapping
-    public String updateByURL(MultipartFile multipartFile, String url) throws IOException {
-        return s3Service.updateByURL(multipartFile,url);
+    public ResponseEntity<ImageUpdateResponseDto> updateByURL(
+            @RequestBody MultipartFile multipartFile, @RequestParam String url)
+            throws IOException {
+        HttpHeaders headers= new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+        ImageUpdateResponseDto dto = new ImageUpdateResponseDto(s3Service.deleteByURL(url));
+
+        return new ResponseEntity<>(dto, headers, HttpStatus.OK);
     }
 
 }

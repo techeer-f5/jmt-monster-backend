d1d1bba87834b31c2212c908d253db7d276986d7
litsynp <nocte55is@gmail.com>
2022-05-25 02:27:30 +0900

    test: increase friend domain DAO coverage to 100%

--- src/test/java/com/techeer/f5/jmtmonster/domain/friend/dao/FriendQueryRepositoryTest.java
+++ src/test/java/com/techeer/f5/jmtmonster/domain/friend/dao/FriendQueryRepositoryTest.java
@@ -6,9 +6,14 @@ import com.techeer.f5.jmtmonster.domain.friend.domain.Friend;
 import com.techeer.f5.jmtmonster.domain.user.domain.User;
 import com.techeer.f5.jmtmonster.domain.user.repository.UserRepository;
 import com.techeer.f5.jmtmonster.global.config.QuerydslConfig;
+import java.util.UUID;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
@@ -30,164 +35,98 @@ class FriendQueryRepositoryTest {
     @DisplayName("친구 검색")
     class SearchFriendsTest {
 
-        @Test
-        @DisplayName("전체 조건")
-        void testSearchFriends() {
-            // given
-            User user1 = userRepository.save(User.builder()
+        private static User user1;
+        private static User user2;
+        private static Friend friend1;
+        private static Friend friend2;
+
+        @BeforeEach
+        void setUp() {
+            user1 = userRepository.save(User.builder()
                     .email("tester1@example.com")
                     .name("Tester1")
                     .nickname("Tester1")
                     .build());
 
-            User user2 = userRepository.save(User.builder()
+            user2 = userRepository.save(User.builder()
                     .email("tester2@example.com")
                     .name("Tester2")
                     .nickname("Tester2")
                     .build());
 
-            Friend friend1 = friendRepository.save(Friend.builder()
+            friend1 = friendRepository.save(Friend.builder()
                     .fromUser(user1)
                     .toUser(user2)
                     .isHangingOut(false)
                     .build());
 
-            Friend friend2 = friendRepository.save(Friend.builder()
+            friend2 = friendRepository.save(Friend.builder()
                     .fromUser(user2)
                     .toUser(user1)
                     .isHangingOut(false)
                     .build());
+        }
+
+        @ParameterizedTest
+        @MethodSource("paramsForSearchFriends_shouldGetResults_ok")
+        @DisplayName("조건 일치 - 성공")
+        void searchFriends_shouldGetResults_ok(boolean useFromUserIdCond, boolean useToUserIdCond,
+                Boolean isHangingOutCond) {
+            // given
+            UUID fromUserId = useFromUserIdCond ? user1.getId() : null;
+            UUID toUserId = useToUserIdCond ? user2.getId() : null;
 
             // when
             Page<Friend> result = friendRepository.searchFriends(
                     PageRequest.of(0, 10),
-                    user1.getId(),
-                    user2.getId(),
-                    false);
+                    fromUserId,
+                    toUserId,
+                    isHangingOutCond);
 
             // then
-            System.out.println(result);
             assertThat(result.stream().toList().get(0).getId()).isEqualTo(friend1.getId());
         }
 
-        @Test
-        @DisplayName("fromUserId 제외 조건")
-        void testSearchFriends_withOutFromUserId() {
+        @ParameterizedTest
+        @MethodSource("paramsForSearchFriends_shouldGetNoResult_ok")
+        @DisplayName("조건 불일치 - 0개 조회")
+        void searchFriends_shouldGetNoResult_ok(boolean useFromUserIdCond, boolean useToUserIdCond,
+                Boolean isHangingOutCond) {
             // given
-            User user1 = userRepository.save(User.builder()
-                    .email("tester1@example.com")
-                    .name("Tester1")
-                    .nickname("Tester1")
-                    .build());
-
-            User user2 = userRepository.save(User.builder()
-                    .email("tester2@example.com")
-                    .name("Tester2")
-                    .nickname("Tester2")
-                    .build());
-
-            Friend friend1 = friendRepository.save(Friend.builder()
-                    .fromUser(user1)
-                    .toUser(user2)
-                    .isHangingOut(false)
-                    .build());
-
-            Friend friend2 = friendRepository.save(Friend.builder()
-                    .fromUser(user2)
-                    .toUser(user1)
-                    .isHangingOut(false)
-                    .build());
+            UUID fromUserId = useFromUserIdCond ? user1.getId() : null;
+            UUID toUserId = useToUserIdCond ? user2.getId() : null;
 
             // when
             Page<Friend> result = friendRepository.searchFriends(
                     PageRequest.of(0, 10),
-                    null,
-                    user2.getId(),
-                    false);
+                    fromUserId,
+                    toUserId,
+                    isHangingOutCond);
 
             // then
-            System.out.println(result);
-            assertThat(result.stream().toList().get(0).getId()).isEqualTo(friend1.getId());
+            assertThat(result).isEmpty();
         }
 
-        @Test
-        @DisplayName("toUserId 제외 조건")
-        void testSearchFriends_withOutToUserId() {
-            // given
-            User user1 = userRepository.save(User.builder()
-                    .email("tester1@example.com")
-                    .name("Tester1")
-                    .nickname("Tester1")
-                    .build());
-
-            User user2 = userRepository.save(User.builder()
-                    .email("tester2@example.com")
-                    .name("Tester2")
-                    .nickname("Tester2")
-                    .build());
-
-            Friend friend1 = friendRepository.save(Friend.builder()
-                    .fromUser(user1)
-                    .toUser(user2)
-                    .isHangingOut(false)
-                    .build());
-
-            Friend friend2 = friendRepository.save(Friend.builder()
-                    .fromUser(user2)
-                    .toUser(user1)
-                    .isHangingOut(false)
-                    .build());
-
-            // when
-            Page<Friend> result = friendRepository.searchFriends(
-                    PageRequest.of(0, 10),
-                    user1.getId(),
-                    null,
-                    false);
-
-            // then
-            System.out.println(result);
-            assertThat(result.stream().toList().get(0).getId()).isEqualTo(friend1.getId());
+        private static Stream<Arguments> paramsForSearchFriends_shouldGetResults_ok() {
+            return Stream.of(
+                    Arguments.of(true, true, false),
+                    Arguments.of(true, true, null),
+                    Arguments.of(true, false, false),
+                    Arguments.of(true, false, null),
+                    Arguments.of(false, true, false),
+                    Arguments.of(false, true, null),
+                    Arguments.of(false, false, null),
+                    Arguments.of(false, false, false)
+            );
         }
 
-        @Test
-        @DisplayName("isHangingOut 제외 조건")
-        void testSearchFriends_withOut() {
-            // given
-            User user1 = userRepository.save(User.builder()
-                    .email("tester1@example.com")
-                    .name("Tester1")
-                    .nickname("Tester1")
-                    .build());
-
-            User user2 = userRepository.save(User.builder()
-                    .email("tester2@example.com")
-                    .name("Tester2")
-                    .nickname("Tester2")
-                    .build());
-
-            Friend friend1 = friendRepository.save(Friend.builder()
-                    .fromUser(user1)
-                    .toUser(user2)
-                    .isHangingOut(false)
-                    .build());
-
-            Friend friend2 = friendRepository.save(Friend.builder()
-                    .fromUser(user2)
-                    .toUser(user1)
-                    .isHangingOut(false)
-                    .build());
-
-            // when
-            Page<Friend> result = friendRepository.searchFriends(
-                    PageRequest.of(0, 10),
-                    user1.getId(),
-                    user2.getId(),
-                    null);
-
-            // then
-            System.out.println(result);
-            assertThat(result.stream().toList().get(0).getId()).isEqualTo(friend1.getId());
+        private static Stream<Arguments> paramsForSearchFriends_shouldGetNoResult_ok() {
+            return Stream.of(
+                    Arguments.of(true, true, true),
+                    Arguments.of(true, false, true),
+                    Arguments.of(false, true, true),
+                    Arguments.of(false, false, true)
+            );
         }
     }
 }
--- src/test/java/com/techeer/f5/jmtmonster/domain/friend/dao/FriendRequestQueryRepositoryTest.java
+++ src/test/java/com/techeer/f5/jmtmonster/domain/friend/dao/FriendRequestQueryRepositoryTest.java
@@ -7,9 +7,14 @@ import com.techeer.f5.jmtmonster.domain.friend.domain.FriendRequestStatus;
 import com.techeer.f5.jmtmonster.domain.user.domain.User;
 import com.techeer.f5.jmtmonster.domain.user.repository.UserRepository;
 import com.techeer.f5.jmtmonster.global.config.QuerydslConfig;
+import java.util.UUID;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
@@ -31,164 +36,98 @@ class FriendRequestQueryRepositoryTest {
     @DisplayName("친구 요청 검색")
     class SearchFriendsTest {
 
-        @Test
-        @DisplayName("전체 조건")
-        void testSearchFriends_allQueries() {
-            // given
-            User user1 = userRepository.save(User.builder()
+        private static User user1;
+        private static User user2;
+        private static FriendRequest friendRequest1;
+        private static FriendRequest friendRequest2;
+
+        @BeforeEach
+        void setUp() {
+            user1 = userRepository.save(User.builder()
                     .email("tester1@example.com")
                     .name("Tester1")
                     .nickname("Tester1")
                     .build());
 
-            User user2 = userRepository.save(User.builder()
+            user2 = userRepository.save(User.builder()
                     .email("tester2@example.com")
                     .name("Tester2")
                     .nickname("Tester2")
                     .build());
 
-            FriendRequest friend1 = friendRequestRepository.save(FriendRequest.builder()
+            friendRequest1 = friendRequestRepository.save(FriendRequest.builder()
                     .fromUser(user1)
                     .toUser(user2)
                     .status(FriendRequestStatus.ACCEPTED)
                     .build());
 
-            FriendRequest friend2 = friendRequestRepository.save(FriendRequest.builder()
+            friendRequest2 = friendRequestRepository.save(FriendRequest.builder()
                     .fromUser(user2)
                     .toUser(user1)
                     .status(FriendRequestStatus.ACCEPTED)
                     .build());
-
-            // when
-            Page<FriendRequest> result = friendRequestRepository.searchFriendRequests(
-                    PageRequest.of(0, 10),
-                    user1.getId(),
-                    user2.getId(),
-                    FriendRequestStatus.ACCEPTED);
-
-            // then
-            System.out.println(result);
-            assertThat(result.stream().toList().get(0).getId()).isEqualTo(friend1.getId());
         }
 
-        @Test
-        @DisplayName("fromUserId 제외 조건")
-        void testSearchFriends_withOutFromUserId() {
+        @ParameterizedTest
+        @MethodSource("paramsForSearchFriendRequests_shouldGetResults_ok")
+        @DisplayName("조건 일치 - 성공")
+        void searchFriendRequests_shouldGetResults_ok(boolean useFromUserIdCond,
+                boolean useToUserIdCond, FriendRequestStatus statusCond) {
             // given
-            User user1 = userRepository.save(User.builder()
-                    .email("tester1@example.com")
-                    .name("Tester1")
-                    .nickname("Tester1")
-                    .build());
-
-            User user2 = userRepository.save(User.builder()
-                    .email("tester2@example.com")
-                    .name("Tester2")
-                    .nickname("Tester2")
-                    .build());
-
-            FriendRequest friend1 = friendRequestRepository.save(FriendRequest.builder()
-                    .fromUser(user1)
-                    .toUser(user2)
-                    .status(FriendRequestStatus.ACCEPTED)
-                    .build());
-
-            FriendRequest friend2 = friendRequestRepository.save(FriendRequest.builder()
-                    .fromUser(user2)
-                    .toUser(user1)
-                    .status(FriendRequestStatus.ACCEPTED)
-                    .build());
+            UUID fromUserId = useFromUserIdCond ? user1.getId() : null;
+            UUID toUserId = useToUserIdCond ? user2.getId() : null;
 
             // when
             Page<FriendRequest> result = friendRequestRepository.searchFriendRequests(
                     PageRequest.of(0, 10),
-                    null,
-                    user2.getId(),
-                    FriendRequestStatus.ACCEPTED);
+                    fromUserId,
+                    toUserId,
+                    statusCond);
 
             // then
-            System.out.println(result);
-            assertThat(result.stream().toList().get(0).getId()).isEqualTo(friend1.getId());
+            assertThat(result.stream().toList().get(0).getId()).isEqualTo(friendRequest1.getId());
         }
 
-        @Test
-        @DisplayName("toUserId 제외 조건")
-        void testSearchFriends_withOutToserId() {
+        @ParameterizedTest
+        @MethodSource("paramsForSearchFriendRequests_shouldGetNoResult_ok")
+        @DisplayName("조건 불일치 - 성공")
+        void searchFriendRequests_shouldGetNoResult_ok(boolean useFromUserIdCond,
+                boolean useToUserIdCond, FriendRequestStatus statusCond) {
             // given
-            User user1 = userRepository.save(User.builder()
-                    .email("tester1@example.com")
-                    .name("Tester1")
-                    .nickname("Tester1")
-                    .build());
-
-            User user2 = userRepository.save(User.builder()
-                    .email("tester2@example.com")
-                    .name("Tester2")
-                    .nickname("Tester2")
-                    .build());
-
-            FriendRequest friend1 = friendRequestRepository.save(FriendRequest.builder()
-                    .fromUser(user1)
-                    .toUser(user2)
-                    .status(FriendRequestStatus.ACCEPTED)
-                    .build());
-
-            FriendRequest friend2 = friendRequestRepository.save(FriendRequest.builder()
-                    .fromUser(user2)
-                    .toUser(user1)
-                    .status(FriendRequestStatus.ACCEPTED)
-                    .build());
+            UUID fromUserId = useFromUserIdCond ? user1.getId() : null;
+            UUID toUserId = useToUserIdCond ? user2.getId() : null;
 
             // when
             Page<FriendRequest> result = friendRequestRepository.searchFriendRequests(
                     PageRequest.of(0, 10),
-                    user1.getId(),
-                    null,
-                    FriendRequestStatus.ACCEPTED);
+                    fromUserId,
+                    toUserId,
+                    statusCond);
 
             // then
-            System.out.println(result);
-            assertThat(result.stream().toList().get(0).getId()).isEqualTo(friend1.getId());
+            assertThat(result).isEmpty();
         }
 
-        @Test
-        @DisplayName("FriendRequestStatus 제외 조건")
-        void testSearchFriends_withOutFriendRequestStatus() {
-            // given
-            User user1 = userRepository.save(User.builder()
-                    .email("tester1@example.com")
-                    .name("Tester1")
-                    .nickname("Tester1")
-                    .build());
+        private static Stream<Arguments> paramsForSearchFriendRequests_shouldGetResults_ok() {
+            return Stream.of(
+                    Arguments.of(true, true, FriendRequestStatus.ACCEPTED),
+                    Arguments.of(true, true, null),
+                    Arguments.of(false, false, FriendRequestStatus.ACCEPTED),
+                    Arguments.of(false, false, null),
+                    Arguments.of(true, false, FriendRequestStatus.ACCEPTED),
+                    Arguments.of(true, false, null),
+                    Arguments.of(false, true, FriendRequestStatus.ACCEPTED),
+                    Arguments.of(false, true, null)
+            );
+        }
 
-            User user2 = userRepository.save(User.builder()
-                    .email("tester2@example.com")
-                    .name("Tester2")
-                    .nickname("Tester2")
-                    .build());
-
-            FriendRequest friend1 = friendRequestRepository.save(FriendRequest.builder()
-                    .fromUser(user1)
-                    .toUser(user2)
-                    .status(FriendRequestStatus.ACCEPTED)
-                    .build());
-
-            FriendRequest friend2 = friendRequestRepository.save(FriendRequest.builder()
-                    .fromUser(user2)
-                    .toUser(user1)
-                    .status(FriendRequestStatus.ACCEPTED)
-                    .build());
-
-            // when
-            Page<FriendRequest> result = friendRequestRepository.searchFriendRequests(
-                    PageRequest.of(0, 10),
-                    user1.getId(),
-                    user2.getId(),
-                    null);
-
-            // then
-            System.out.println(result);
-            assertThat(result.stream().toList().get(0).getId()).isEqualTo(friend1.getId());
+        private static Stream<Arguments> paramsForSearchFriendRequests_shouldGetNoResult_ok() {
+            return Stream.of(
+                    Arguments.of(true, true, FriendRequestStatus.PENDING),
+                    Arguments.of(false, false, FriendRequestStatus.PENDING),
+                    Arguments.of(true, false, FriendRequestStatus.PENDING),
+                    Arguments.of(false, true, FriendRequestStatus.PENDING)
+            );
         }
     }
 }

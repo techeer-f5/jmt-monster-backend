c182c30b21233771cb28384282e3720df74f4054
Wooseok Choe <chldntjr0425@gmail.com>
2022-06-04 00:08:16 +0900

    feat: add S3 response dto

---
+++ src/main/java/com/techeer/f5/jmtmonster/domain/review/dto/response/S3ResponseDto.java
@@ -0,0 +1,19 @@
+package com.techeer.f5.jmtmonster.domain.review.dto.response;
+
+
+import com.techeer.f5.jmtmonster.global.domain.dto.BaseTimeEntityDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class S3ResponseDto extends BaseTimeEntityDto {
+
+    @NotNull
+    private String url;
+
+}

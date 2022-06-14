5d0cc013601930d19b443c1f7f52848689010382
jiho lee <optional.int@kakao.com>
2022-06-15 00:46:43 +0900

    feat: add HomeService

---
+++ src/main/java/com/techeer/f5/jmtmonster/domain/home/service/HomeService.java
@@ -0,0 +1,24 @@
+package com.techeer.f5.jmtmonster.domain.home.service;
+
+import com.techeer.f5.jmtmonster.domain.home.dto.HomeHistoriesDto;
+import com.techeer.f5.jmtmonster.domain.home.dto.HomeResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HomeService {
+
+    public HomeResponseDto getHome() {
+        // FIXME: implementation needed
+        return null;
+    }
+
+    public HomeHistoriesDto getHomeHistory() {
+        // FIXME: implementation needed
+        return null;
+    }
+
+    public HomeResponseDto migrate() {
+        // FIXME: implementation needed
+        return null;
+    }
+}

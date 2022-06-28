05018e8a888f49cf695055bf4538b5d02917a36e
litsynp <nocte55is@gmail.com>
2022-06-29 00:25:42 +0900

    fix: set alias for friend queries

--- src/main/java/com/techeer/f5/jmtmonster/domain/friend/dao/FriendRequestQueryRepositoryImpl.java
+++ src/main/java/com/techeer/f5/jmtmonster/domain/friend/dao/FriendRequestQueryRepositoryImpl.java
@@ -1,13 +1,13 @@
 package com.techeer.f5.jmtmonster.domain.friend.dao;
 
 import static com.techeer.f5.jmtmonster.domain.friend.domain.QFriendRequest.friendRequest;
-import static com.techeer.f5.jmtmonster.domain.user.domain.QUser.user;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.techeer.f5.jmtmonster.domain.friend.domain.FriendRequest;
 import com.techeer.f5.jmtmonster.domain.friend.domain.FriendRequestStatus;
+import com.techeer.f5.jmtmonster.domain.user.domain.QUser;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -25,37 +25,45 @@ public class FriendRequestQueryRepositoryImpl implements FriendRequestQueryRepos
     @Override
     public Page<FriendRequest> searchFriendRequests(Pageable pageable, UUID fromUserIdCond,
             UUID toUserIdCond, FriendRequestStatus statusCond) {
+        QUser fromUser = new QUser("fromUser");
+        QUser toUser = new QUser("toUser");
+
         List<FriendRequest> query = jpaQueryFactory
                 .selectFrom(friendRequest)
-                .innerJoin(friendRequest.fromUser, user)
+                .innerJoin(friendRequest.fromUser, fromUser)
                 .fetchJoin()
-                .innerJoin(friendRequest.toUser, user)
+                .innerJoin(friendRequest.toUser, toUser)
                 .fetchJoin()
-                .where(fromUserIdEq(fromUserIdCond),
-                        toUserIdEq(toUserIdCond),
+                .where(fromUserIdEq(fromUser, fromUserIdCond),
+                        toUserIdEq(toUser, toUserIdCond),
                         statusEq(statusCond))
                 .fetch();
 
-        JPAQuery<Long> countQuery = getCount();
+        JPAQuery<Long> countQuery = getCount(fromUser, toUser, fromUserIdCond, toUserIdCond,
+                statusCond);
 
         return PageableExecutionUtils.getPage(query, pageable, countQuery::fetchOne);
     }
 
-    private BooleanExpression fromUserIdEq(UUID fromUserIdCond) {
-        return fromUserIdCond != null ? friendRequest.fromUser.id.eq(fromUserIdCond) : null;
+    private BooleanExpression fromUserIdEq(QUser fromUser, UUID fromUserIdCond) {
+        return fromUserIdCond != null ? fromUser.id.eq(fromUserIdCond) : null;
     }
 
-    private BooleanExpression toUserIdEq(UUID toUserIdCond) {
-        return toUserIdCond != null ? friendRequest.toUser.id.eq(toUserIdCond) : null;
+    private BooleanExpression toUserIdEq(QUser toUser, UUID toUserIdCond) {
+        return toUserIdCond != null ? toUser.id.eq(toUserIdCond) : null;
     }
 
     private BooleanExpression statusEq(FriendRequestStatus statusCond) {
         return statusCond != null ? friendRequest.status.eq(statusCond) : null;
     }
 
-    private JPAQuery<Long> getCount() {
+    private JPAQuery<Long> getCount(QUser fromUser, QUser toUser, UUID fromUserIdCond,
+            UUID toUserIdCond, FriendRequestStatus statusCond) {
         return jpaQueryFactory
                 .select(friendRequest.count())
+                .where(fromUserIdEq(fromUser, fromUserIdCond),
+                        toUserIdEq(toUser, toUserIdCond),
+                        statusEq(statusCond))
                 .from(friendRequest);
     }
 }

212093b3c52773ac2b660f5e0e31cfb0deaa37b5
Wooseok Choe <chldntjr0425@gmail.com>
2022-07-01 18:32:03 +0900

    chore: refactor dto name

--- src/main/java/com/techeer/f5/jmtmonster/domain/review/dao/ReviewRequestRepository.java
+++ src/main/java/com/techeer/f5/jmtmonster/domain/review/dao/ReviewRequestRepository.java
@@ -1,11 +1,10 @@
 package com.techeer.f5.jmtmonster.domain.review.dao;
 
-import com.techeer.f5.jmtmonster.domain.friend.domain.FriendRequest;
-import com.techeer.f5.jmtmonster.domain.review.domain.ReviewRequest;
+import com.techeer.f5.jmtmonster.domain.review.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface ReviewRequestRepository extends JpaRepository<ReviewRequest, UUID> {
+public interface ReviewRequestRepository extends JpaRepository<Review, UUID> {
     boolean existsById(UUID uuid);
 }

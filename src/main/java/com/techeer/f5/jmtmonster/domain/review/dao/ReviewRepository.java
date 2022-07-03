ddf632ce61fcc0e220b7ba51a555ddbe78d9a15b
Wooseok Choe <chldntjr0425@gmail.com>
2022-07-03 17:08:52 +0900

    feat: add findByUserId method for pagination

--- src/main/java/com/techeer/f5/jmtmonster/domain/review/dao/ReviewRepository.java
+++ src/main/java/com/techeer/f5/jmtmonster/domain/review/dao/ReviewRepository.java
@@ -1,10 +1,14 @@
 package com.techeer.f5.jmtmonster.domain.review.dao;
 
 import com.techeer.f5.jmtmonster.domain.review.domain.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
     boolean existsById(UUID uuid);
+    Page<Review> findByUserId(UUID userId, Pageable pageable);
 }

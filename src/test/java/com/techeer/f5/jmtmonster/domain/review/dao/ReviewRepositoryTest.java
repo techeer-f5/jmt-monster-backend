f0bf201a04467acf510e3377df1676c48b1855bc
Wooseok Choe <chldntjr0425@gmail.com>
2022-07-03 17:06:53 +0900

    feat: add FindByUserIdTest code

--- src/test/java/com/techeer/f5/jmtmonster/domain/review/dao/ReviewRepositoryTest.java
+++ src/test/java/com/techeer/f5/jmtmonster/domain/review/dao/ReviewRepositoryTest.java
@@ -4,12 +4,13 @@ import com.techeer.f5.jmtmonster.domain.review.domain.*;
 import com.techeer.f5.jmtmonster.domain.user.domain.User;
 import com.techeer.f5.jmtmonster.domain.user.repository.UserRepository;
 import com.techeer.f5.jmtmonster.global.config.QuerydslConfig;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.UUID;
@@ -33,62 +34,125 @@ public class ReviewRepositoryTest {
     @Autowired
     private ReviewRepository reviewRepository;
 
-    @Test
-    @DisplayName("Review ID를 이용한 검색")
-    void existsByIdTest() {
-        // given
-        User user;
-        ReviewFood food1;
-        ReviewFood food2;
-        List<ReviewFood> foods;
-        ReviewImage image1;
-        ReviewImage image2;
-        List<ReviewImage> images;
-        Review review;
+    private User givenUser;
+    private ReviewFood givenFood1;
+    private ReviewFood givenFood2;
+    private List<ReviewFood> givenFoods1;
+    private ReviewImage givenImage1;
+    private ReviewImage givenImage2;
+    private List<ReviewImage> givenImages1;
+    private Review givenReview1;
 
-        user = userRepository.save(User.builder()
+    private ReviewFood givenFood3;
+    private ReviewFood givenFood4;
+    private List<ReviewFood> givenFoods2;
+    private ReviewImage givenImage3;
+    private ReviewImage givenImage4;
+    private List<ReviewImage> givenImages2;
+    private Review givenReview2;
+
+    @BeforeEach
+    void setUp(){
+        givenUser = userRepository.save(User.builder()
                 .email("tester1@example.com")
                 .name("Tester1")
                 .nickname("Tester1")
                 .build());
 
-        review = reviewRepository.save(Review.builder()
-                .user(user)
-                .content("Test content")
+        // Review 1
+        givenReview1 = reviewRepository.save(Review.builder()
+                .user(givenUser)
+                .content("Test content 1")
                 .like(Like.LIKE)
                 .star(Star.FIVE)
                 .foodList(null)
                 .imageList(null)
                 .build());
-
-        food1 = foodRepository.save(ReviewFood.builder()
-                .review(review)
+        givenFood1 = foodRepository.save(ReviewFood.builder()
+                .review(givenReview1)
                 .food("Test food 1")
                 .build());
-        food2 = foodRepository.save(ReviewFood.builder()
-                .review(review)
+        givenFood2 = foodRepository.save(ReviewFood.builder()
+                .review(givenReview1)
                 .food("Test food 2")
                 .build());
-        foods = List.of(food1, food2);
-
-        image1 = imageRepository.save(ReviewImage.builder()
-                .review(review)
+        givenFoods1 = List.of(givenFood1, givenFood2);
+        givenImage1 = imageRepository.save(ReviewImage.builder()
+                .review(givenReview1)
                 .url("Test URL 1")
                 .build());
-        image2 = imageRepository.save(ReviewImage.builder()
-                .review(review)
+        givenImage2 = imageRepository.save(ReviewImage.builder()
+                .review(givenReview1)
                 .url("Test URL 2")
                 .build());
-        images = List.of(image1, image2);
+        givenImages1 = List.of(givenImage1, givenImage2);
+        givenReview1.setFoodList(givenFoods1);
+        givenReview1.setImageList(givenImages1);
 
-        review.setFoodList(foods);
-        review.setImageList(images);
+        //Review 2
+        givenReview2 = reviewRepository.save(Review.builder()
+                .user(givenUser)
+                .content("Test content 2")
+                .like(Like.LIKE)
+                .star(Star.FIVE)
+                .foodList(null)
+                .imageList(null)
+                .build());
+        givenFood3 = foodRepository.save(ReviewFood.builder()
+                .review(givenReview2)
+                .food("Test food 3")
+                .build());
+        givenFood4 = foodRepository.save(ReviewFood.builder()
+                .review(givenReview2)
+                .food("Test food 4")
+                .build());
+        givenFoods2 = List.of(givenFood3, givenFood4);
+        givenImage3 = imageRepository.save(ReviewImage.builder()
+                .review(givenReview2)
+                .url("Test URL 3")
+                .build());
+        givenImage4 = imageRepository.save(ReviewImage.builder()
+                .review(givenReview2)
+                .url("Test URL 4")
+                .build());
+        givenImages2 = List.of(givenImage3, givenImage4);
+        givenReview2.setFoodList(givenFoods2);
+        givenReview2.setImageList(givenImages2);
+    }
 
-        // when
-        boolean actualResult = reviewRepository.existsById(review.getId());
-        boolean expectedResult = true;
+    @Nested
+    @DisplayName("Review ID를 이용한 검색")
+    class ExistsByIdTest{
+        @Test
+        @DisplayName("성공")
+        void existsByIdSuccessTest() {
+            // given
 
-        // then
-        assertThat(actualResult).isEqualTo(expectedResult);
+            // when
+            boolean actualResult = reviewRepository.existsById(givenReview1.getId());
+            boolean expectedResult = true;
+
+            // then
+            assertThat(actualResult).isEqualTo(expectedResult);
+        }
+    }
+
+    @Nested
+    @DisplayName("USER ID를 이용하여 REVIEW 리스트 검색")
+    class FindByUserIdTest{
+        @Test
+        @DisplayName("성공")
+        void FindByUserIdSuccessTest() {
+            // given
+            UUID userId= givenUser.getId();
+            Pageable pageable = PageRequest.of(0,10);
+
+            // when
+            Page<Review> result = reviewRepository.findByUserId(userId, pageable);
+
+            // then
+            assertThat(result.stream().toList().size()).isEqualTo(2);
+        }
+
     }
 }

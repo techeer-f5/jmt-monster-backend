c069746fcb7101c21acc764dba5a3c68de663c2b
Wooseok Choe <chldntjr0425@gmail.com>
2022-07-04 23:45:03 +0900

    chore: delete unused dependency

--- src/main/java/com/techeer/f5/jmtmonster/domain/review/dao/ReviewRepository.java
+++ src/main/java/com/techeer/f5/jmtmonster/domain/review/dao/ReviewRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface ReviewRepository extends JpaRepository<Review, UUID> {

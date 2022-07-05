3476a9d568443dfb9f22517a2a0cfb5beafb1efe
Wooseok Choe <chldntjr0425@gmail.com>
2022-07-05 21:52:01 +0900

    refactor: make convert to use Files.createTempDir

--- src/main/java/com/techeer/f5/jmtmonster/s3/util/S3Manager.java
+++ src/main/java/com/techeer/f5/jmtmonster/s3/util/S3Manager.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.aventrix.jnanoid.jnanoid.NanoIdUtils;
+import com.google.common.io.Files;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -65,7 +66,7 @@ public class S3Manager {
 
     // 로컬에 파일 업로드 하기
     private Optional<File> convert(MultipartFile file) throws IOException {
-        File convertFile = new File(System.getProperty("user.dir") + "/" + file.getOriginalFilename());
+        File convertFile = new File( Files.createTempDir().getAbsolutePath() + "/" + file.getOriginalFilename());
         if (convertFile.createNewFile()) { // 바로 위에서 지정한 경로에 File이 생성됨 (경로가 잘못되었다면 생성 불가능)
             try (FileOutputStream fos = new FileOutputStream(convertFile)) { // FileOutputStream 데이터를 파일에 바이트 스트림으로 저장하기 위함
                 fos.write(file.getBytes());

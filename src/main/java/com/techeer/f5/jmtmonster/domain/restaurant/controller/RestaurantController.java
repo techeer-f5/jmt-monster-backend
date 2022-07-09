85afdd774919d87686ffc41c0eaa8f467346bc08
gilhan <rlfgks97@gmail.com>
2022-07-09 19:04:50 +0900

    feat: set basic feature for restaurant Controller

--- src/main/java/com/techeer/f5/jmtmonster/domain/restaurant/controller/RestaurantController.java
+++ src/main/java/com/techeer/f5/jmtmonster/domain/restaurant/controller/RestaurantController.java
@@ -1,6 +1,6 @@
 package com.techeer.f5.jmtmonster.domain.restaurant.controller;
 
-import com.techeer.f5.jmtmonster.domain.restaurant.dto.response.MenuInfoResponseDto;
+import com.techeer.f5.jmtmonster.domain.restaurant.dto.response.RestaurantResponseDto;
 import com.techeer.f5.jmtmonster.domain.restaurant.service.MenuService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,12 +14,17 @@ public class RestaurantController {
     private final MenuService menuService;
 
     @GetMapping("/{cid}")
-    public ResponseEntity<MenuInfoResponseDto> getRestaurantInformation(@PathVariable Long cid) {
-        MenuInfoResponseDto menuInfoResponseDto = menuService.getMenuByKakao(cid);
+    public ResponseEntity<RestaurantResponseDto> getRestaurantInformation(@PathVariable Long cid) {
+        RestaurantResponseDto restaurantInfo = menuService.getRestaurantInfo(cid);
 
+
+
+//        요청한 cid가 기존에 존재하는 데이터일 시, HTTP status return 변경
+//        return ResponseEntity.status(HttpStatus.OK)
+//                .body(menuInfoResponseDto);
 
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(menuInfoResponseDto);
+                .body(restaurantInfo);
     }
 
 }
